ba547b7ff8c69da07994d6bf7b12a6857a63d851
Sami Fournier <sami.fournier.sf@gmail.com>
2020-01-07 15:17:35 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+const scriptURL = 'https://hooks.zapier.com/hooks/catch/5385899/otlarqc/'
+let alreadyDone = false;
+
+const form1 = document.forms['formHeader']
+form1.addEventListener('submit', e => {
+  e.preventDefault();
+  $('#exampleModalCenter').modal('toggle');
+  if(!alreadyDone){
+    document.getElementById('modal_loader').classList.remove("d-none");
+    fetch(scriptURL, { method: 'POST', body: new FormData(form1)})
+    .then(response => {
+      document.getElementById('modal_loader').classList.add("d-none");
+      document.getElementById('modal_success').classList.remove("d-none");
+      alreadyDone = true;
+    })
+    .catch(error => {
+      console.error('Error!', error.message);
+      document.getElementById('modal_loader').classList.add("d-none");
+      document.getElementById('modal_error').classList.remove("d-none");
+    })
+  }
+})
+
+
+const form2 = document.forms['formPrice']
+form2.addEventListener('submit', e => {
+  e.preventDefault();
+  $('#exampleModalCenter').modal('toggle');
+  if(!alreadyDone){
+    document.getElementById('modal_loader').classList.remove("d-none");
+    fetch(scriptURL, { method: 'POST', body: new FormData(form2)})
+    .then(response => {
+      document.getElementById('modal_loader').classList.add("d-none");
+      document.getElementById('modal_success').classList.remove("d-none");
+      alreadyDone = true;
+    })
+    .catch(error => {
+      console.error('Error!', error.message);
+      document.getElementById('modal_loader').classList.add("d-none");
+      document.getElementById('modal_error').classList.remove("d-none");
+    })
+  }
+})
